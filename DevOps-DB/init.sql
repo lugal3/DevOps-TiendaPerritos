707fff93eca9796c14c3c5c875ef77e78cd95e1e
lugal3 <l.ugal3.dev@gmail.com>
2026-05-26 14:28:02 -0400

    pruebas pipelines

--- DevOps-DB/init.sql
+++ DevOps-DB/init.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS tienda_perritos;
 USE tienda_perritos;
 
 CREATE TABLE IF NOT EXISTS productos (
+    
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     descripcion VARCHAR(255),
